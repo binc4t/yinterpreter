a02e9061c361d3a7cbeaaf60f77093b28ce0fe71
binc4t <y980715@gmail.com>
2024-09-30 17:46:56 +0800

    feat: more op

--- identify/const.go
+++ identify/const.go
@@ -7,14 +7,21 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	FOR      = "FOR"
+	RETURN   = "RETURN"
 
-	OPAssign = "="
-	OPPlus   = "+"
-	OPMinus  = "-"
-	OPGT     = ">"
-	OPLT     = "<"
-	OPEQ     = "=="
-	OPNEQ    = "!="
+	OPAssign   = "="
+	OPPlus     = "+"
+	OPMinus    = "-"
+	OPAsterisk = "*"
+	OPSlash    = "/"
+	OPGT       = ">"
+	OPLT       = "<"
+	OPBang     = "!"
+	OPEQ       = "=="
+	OPNEQ      = "!="
 
 	LParen    = "("
 	RParen    = ")"
@@ -23,3 +30,36 @@ const (
 	Comma     = ","
 	Semicolon = ";"
 )
+
+var keywords = map[string]string{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"for":    FOR,
+	"return": RETURN,
+
+	"=":  OPEQ,
+	"+":  OPPlus,
+	"-":  OPMinus,
+	"*":  OPAsterisk,
+	"/":  OPSlash,
+	">":  OPGT,
+	"<":  OPLT,
+	"==": OPEQ,
+	"!=": OPNEQ,
+
+	"(": LParen,
+	")": RParen,
+	"{": LBrace,
+	"}": RBrace,
+	",": Comma,
+	";": Semicolon,
+}
+
+func LookupKeywords(s string) string {
+	if k, ok := keywords[s]; ok {
+		return k
+	}
+	return ""
+}
